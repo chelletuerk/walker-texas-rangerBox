d1add51a4f7a55c0e793b63f8ef1fadd978dca94
chelle tuerk <michelletuerk@gmail.com>
2017-01-27 10:53:23 -0700

    15 passing tests

--- app/components/Header/header.spec.js
+++ app/components/Header/header.spec.js
@@ -9,6 +9,8 @@ import Favorites from '../Favorites';
 import SettingsButton from '../Button/SettingsButton';
 import SetButton from '../Button/SetButton';
 import GetJokesButton from '../Button/GetJokesButton'
+import ToJokesButton from '../Button/ToJokesButton'
+import FavoritesButton from '../Button/FavoritesButton'
 
 
 
@@ -62,6 +64,55 @@ describe('Favorites', () => {
     expect(wrapper.find('button')).to.be.length(1);
   });
 });
+
+describe('ToJokesButton', () => {
+  const wrapper = shallow(<ToJokesButton />);
+
+  it('should render as a <button>', () => {
+    assert.equal(wrapper.type(), 'button');
+  });
+
+  it('should render a submit button', () => {
+    expect(wrapper.find('.round-buttons')).to.be.length(1);
+  });
+});
+
+describe('FavoritesButton', () => {
+  const wrapper = shallow(<FavoritesButton />);
+
+  it('should render as a <div>', () => {
+    assert.equal(wrapper.type(), 'div');
+  });
+
+  describe('ToJokesButton', () => {
+    const wrapper = shallow(<ToJokesButton />);
+
+    it('should render as a <button>', () => {
+      assert.equal(wrapper.type(), 'button');
+    });
+
+    it('should render a submit button', () => {
+      expect(wrapper.find('.round-buttons')).to.be.length(1);
+    });
+  });
+
+  describe('GetJokesButton', () => {
+    const wrapper = shallow(<GetJokesButton />);
+
+    it('should render as a <div>', () => {
+      assert.equal(wrapper.type(), 'div');
+    });
+
+    it('should render a submit button', () => {
+      expect(wrapper.find('.get-jokes')).to.be.length(1);
+    });
+  });
+
+
+
+
+});
+
 
 describe.skip('Feature Test | AddNewJoke', () => {
   it('submits a joke', () => {
