2c90367f2ff774609c91e82ce4c86551c3939f86
chelle tuerk <michelletuerk@gmail.com>
2017-01-27 10:42:23 -0700

    8 passing tests

--- app/components/Header/header.spec.js
+++ app/components/Header/header.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import {expect} from 'chai';
+import { mount, shallow, render } from 'enzyme';
+import {expect, assert} from 'chai';
 import sinon from 'sinon'
 
 import App from '../App';
-import Button from '../Button/SettingsButton';
+import JokeNumInput from '../JokeNumInput';
+import Favorites from '../Favorites';
+import SettingsButton from '../Button/SettingsButton';
+import SetButton from '../Button/SetButton';
+import GetJokesButton from '../Button/GetJokesButton'
+
+
 
 describe('<App/>', () => {
   it('should display a single h1 tag', () => {
@@ -18,11 +24,53 @@ describe('<App/>', () => {
     expect(wrapper.find('ul')).to.have.length(1);
   });
 
-  // it('should trigger our logStuff function', () => {
-  //   const logStuff = sinon.spy()
-  //   const wrapper = mount(<Button handleClick={logStuff} />);
-  //   wrapper.simulate('click');
-  //   expect(logStuff.calledOnce).to.equal(true);
-  // });
+    it('should have the button text rendered onto the page', function(){
+    const wrapper = render(<GetJokesButton/>)
+    expect(wrapper.text()).to.contain('New Jokes')
+  })
 
+  });
+})
+
+describe('Settings', () => {
+  const wrapper = shallow(<SetButton />);
+
+  it('should render as a <div>', () => {
+    assert.equal(wrapper.type(), 'div');
+  });
+
+  it('should have props', () => {
+    const wrapper = mount(<SetButton
+      fetchNameJokes = 'name'
+    />);
+    expect(wrapper.prop('fetchNameJokes')).to.equal('name');
+  });
+
+  it('should render 1 button', () => {
+    expect(wrapper.find('button')).to.be.length(1);
+  });
 });
+
+describe('Favorites', () => {
+  const wrapper = shallow(<SetButton />);
+
+  it('should render as a <div>', () => {
+    assert.equal(wrapper.type(), 'div');
+  });
+
+  it('should render 1 button', () => {
+    expect(wrapper.find('button')).to.be.length(1);
+  });
+});
+
+describe.skip('Feature Test | AddNewJoke', () => {
+  it('submits a joke', () => {
+    const buttonClick = sinon.spy()
+    const wrapper = shallow(
+      <GetJokesButton onClick={buttonClick} />)
+    const wrapper2 = shallow(
+      <App />)
+    wrapper.find('.get-jokes').simulate('')
+    expect(wrapper2.state('jokes')).to.eq('click')
+  })
+})
